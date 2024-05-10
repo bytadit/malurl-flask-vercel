b25ac6aa9ab7a22c686b2d98cc871cee5873a674
bytadit <bgsp_aditya@student.uns.ac.id>
2024-05-10 10:09:56 +0700

    nest

--- index.py
+++ index.py
@@ -1,19 +1,18 @@
-import torch
-from transformers import pipeline
-from transformers import AutoTokenizer
-from transformers import AutoModelForSequenceClassification
+from transformers import pipeline, set_seed
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from flask import Flask, request, jsonify
+import torch
 
-device = torch.device("cpu")  # Use CPU device
+set_seed(42)  # Setting a random seed for reproducibility
 
 def predictNewData(link):
     checkpoint = 'bgspaditya/malurl-electra-10e'
     id2label = {0:'benign',1:'defacement',2:'malware',3:'phishing'}
     label2id = {'benign':0,'defacement':1,'malware':2,'phishing':3}
     num_labels=4
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint, use_fast=True)
-    model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=num_labels, id2label=id2label, label2id=label2id)
-    url_classifier = pipeline(task='text-classification', model=model, tokenizer=tokenizer, device=device)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, use_fast=True, force_download=True)
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=num_labels, id2label=id2label, label2id=label2id, force_download=True)
+    url_classifier = pipeline(task='text-classification', model=model, tokenizer=tokenizer)
     result = url_classifier(link)
     return {'label': result[0]['label'], 'score': result[0]['score']}
 
